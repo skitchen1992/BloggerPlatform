82b93a60b1b1abf479b8e32ad4cd876de041306c
nikita <nikita.lavrentyev@mysky.com>
2024-07-12 23:22:07 +0400

    fix logic 17

--- src/middlewares/bearer-token-user_interceptor-middleware.ts
+++ src/middlewares/bearer-token-user_interceptor-middleware.ts
@@ -1,26 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
-import { HTTP_STATUSES } from '../utils/consts';
 import { JwtPayload } from 'jsonwebtoken';
 import { jwtService, userRepository } from '../compositions/composition-root';
 
 export const bearerTokenUserInterceptorMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || authHeader.indexOf('Bearer ') === -1) {
-    res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
-    return;
-  }
+  if (authHeader && authHeader.indexOf('Bearer ') !== -1) {
+    const [_, token] = authHeader.split(' ');
 
-  const [_, token] = authHeader.split(' ');
+    const { userId } = (jwtService.verifyToken(token) as JwtPayload) ?? {};
 
-  const { userId } = (jwtService.verifyToken(token) as JwtPayload) ?? {};
+    const { data: user } = await userRepository.getUserById(userId);
 
-  const { data: user } = await userRepository.getUserById(userId);
-
-  if (user) {
-    res.locals.user = user;
-    next();
+    if (user) {
+      res.locals.user = user;
+      next();
+    } else {
+      res.locals.user = undefined;
+      next();
+    }
   } else {
-    res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401);
+    res.locals.user = undefined;
+    next();
   }
 };
