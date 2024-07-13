e07a5ddbb40f1423c22824494c7d2d7d2c6b739d
nikita <nikita.lavrentyev@mysky.com>
2024-07-13 14:37:41 +0400

    fix logic 18

--- src/app.ts
+++ src/app.ts
@@ -35,3 +35,47 @@ app.use(PATH_URL.AUTH.ROOT, authRouter);
 app.use(PATH_URL.SECURITY.ROOT, securityRouter);
 
 app.use(PATH_URL.TESTING.ROOT, testingRouter);
+
+const a = {
+  'pagesCount': 1,
+  'page': 1,
+  'pageSize': 10,
+  'totalCount': 6,
+  'items': [{
+    'id': '6692472b71eb46e2798037e2',
+    'content': 'length_21-weqweqweqwq',
+    'commentatorInfo': { 'userId': '6692470471eb46e279803670', 'userLogin': '8044lg' },
+    'createdAt': '2024-07-13T09:21:47.000Z',
+    'likesInfo': { 'likesCount': 1, 'dislikesCount': 1, 'myStatus': 'Like' },
+  }, {
+    'id': '6692472a71eb46e2798037d6',
+    'content': 'length_21-weqweqweqwq',
+    'commentatorInfo': { 'userId': '6692470471eb46e279803670', 'userLogin': '8044lg' },
+    'createdAt': '2024-07-13T09:21:46.000Z',
+    'likesInfo': { 'likesCount': 1, 'dislikesCount': 1, 'myStatus': 'None' },
+  }, {
+    'id': '6692472971eb46e2798037ca',
+    'content': 'length_21-weqweqweqwq',
+    'commentatorInfo': { 'userId': '6692470471eb46e279803670', 'userLogin': '8044lg' },
+    'createdAt': '2024-07-13T09:21:45.000Z',
+    'likesInfo': { 'likesCount': 4, 'dislikesCount': 0, 'myStatus': 'Like' },
+  }, {
+    'id': '6692472871eb46e2798037be',
+    'content': 'length_21-weqweqweqwq',
+    'commentatorInfo': { 'userId': '6692470471eb46e279803670', 'userLogin': '8044lg' },
+    'createdAt': '2024-07-13T09:21:44.000Z',
+    'likesInfo': { 'likesCount': 0, 'dislikesCount': 1, 'myStatus': 'Dislike' },
+  }, {
+    'id': '6692472871eb46e2798037b2',
+    'content': 'length_21-weqweqweqwq',
+    'commentatorInfo': { 'userId': '6692470471eb46e279803670', 'userLogin': '8044lg' },
+    'createdAt': '2024-07-13T09:21:44.000Z',
+    'likesInfo': { 'likesCount': 2, 'dislikesCount': 0, 'myStatus': 'None' },
+  }, {
+    'id': '6692472771eb46e2798037a6',
+    'content': 'length_21-weqweqweqwq',
+    'commentatorInfo': { 'userId': '6692470471eb46e279803670', 'userLogin': '8044lg' },
+    'createdAt': '2024-07-13T09:21:43.000Z',
+    'likesInfo': { 'likesCount': 2, 'dislikesCount': 0, 'myStatus': 'Like' },
+  }],
+};
